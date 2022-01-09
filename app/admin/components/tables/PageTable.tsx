537ed982a0c66d8f01fa4cbe297fea6caedf7d5e
guido offermans <guidooffermans@hotmail.com>
2022-01-09 23:08:55 +0100

    add a PageTable

---
+++ app/admin/components/tables/PageTable.tsx
@@ -0,0 +1,69 @@
+const pages = [
+  {
+    title: "Home Page",
+    status: "Published",
+  },
+  {
+    title: "Portfolio Page",
+    status: "Published",
+  },
+  {
+    title: "Test Page",
+    status: "Published",
+  },
+]
+
+const PageTable = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-0 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {pages.map((page) => (
+                  <tr key={page.title}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{page.title}</div>
+                    </td>
+                    <td className="px-0 w-4 py-4 whitespace-nowrap">
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        {page.status}
+                      </span>
+                    </td>
+                    <td className="px-1 py-4  whitespace-nowrap text-center text-sm font-medium">
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PageTable
--- app/admin/pages/Pages.tsx
+++ app/admin/pages/Pages.tsx
@@ -1,10 +1,17 @@
 import { BlitzPage } from "blitz"
 
 import AdminLayout from "../../admin/layouts/AdminLayout"
-import ActionHeader from "../components/headings/ActionHeader"
+import ActionPageHeading from "../components/headings/ActionPageHeading"
+import PageTable from "../components/tables/PageTable"
 
 const ManagePages: BlitzPage = () => {
-  return <ActionHeader title={"test"} actionName={"click"} />
+  return (
+    <div className="p-4">
+      <ActionPageHeading title={"Edit your pages here"} actionName={"click"} />
+      <div className="py-4" />
+      <PageTable />
+    </div>
+  )
 }
 
 ManagePages.suppressFirstRenderFlicker = true
