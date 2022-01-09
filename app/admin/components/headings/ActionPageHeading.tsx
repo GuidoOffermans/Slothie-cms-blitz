ab882784b0cfed62512e2367f2eea1ba9d71cb23
guido offermans <guidooffermans@hotmail.com>
2022-01-09 23:07:22 +0100

    add link to adminpage and edit the pageheading

--- app/admin/components/headings/ActionPageHeading.tsx
+++ app/admin/components/headings/ActionPageHeading.tsx
@@ -11,6 +11,9 @@ const ActionPageHeading = ({ title, actionName, action }: ActionPageHeadingProps
         <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
           {title}
         </h2>
+        <p className="mt-1 text-sm text-gray-500">
+          You can check the status of your pages and edit them here.
+        </p>
       </div>
       <div className="mt-4 flex md:mt-0 md:ml-4">
         <button
@@ -23,7 +26,7 @@ const ActionPageHeading = ({ title, actionName, action }: ActionPageHeadingProps
           type="button"
           className="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
-          Publish
+          Create page
         </button>
       </div>
     </div>
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,54 +1,17 @@
-import { Link, BlitzPage, useMutation, Routes } from "blitz"
+import { BlitzPage, Link, Routes } from "blitz"
 
 import Layout from "app/core/layouts/Layout"
-import { useCurrentUser } from "app/core/hooks/useCurrentUser"
-import logout from "app/auth/mutations/logout"
-
-const UserInfo = () => {
-  const currentUser = useCurrentUser()
-  const [logoutMutation] = useMutation(logout)
-
-  if (currentUser) {
-    return (
-      <>
-        <button
-          className="button small"
-          onClick={async () => {
-            await logoutMutation()
-          }}
-        >
-          Logout
-        </button>
-        <div>
-          User id: <code>{currentUser.id}</code>
-          <br />
-          User role: <code>{currentUser.role}</code>
-        </div>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <Link href={Routes.SignupPage()}>
-          <a className="button small">
-            <strong>Sign Up</strong>
-          </a>
-        </Link>
-        <Link href={Routes.LoginPage()}>
-          <a className="button small">
-            <strong>Login</strong>
-          </a>
-        </Link>
-      </>
-    )
-  }
-}
 
 const Home: BlitzPage = () => {
   return (
     <div className="container">
       <main>
         <h2>Home Page</h2>
+        <Link href={Routes.Admin()}>
+          <a className="text-indigo-600 hover:text-indigo-900 hover:bg-stone-200">
+            Go to the Admin Page
+          </a>
+        </Link>
       </main>
     </div>
   )
